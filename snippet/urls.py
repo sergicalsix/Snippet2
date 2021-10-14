2063533a70970828eeb6ff37dc7095a0c1f50280
sergicalsix <kenp3434membertip1@gmail.com>
2021-10-14 15:21:19 +0900

    add:view

--- snippet/urls.py
+++ snippet/urls.py
@@ -4,6 +4,7 @@ from snippet import views
 
 urlpatterns = [
     path("new/", views.snippet_new, name = "snippet_new"),
-    path("<int:snippet_id>/", views.snippet_detail, name = "snippet_detail"),
+    #path("<int:snippet_id>/", views.snippet_detail, name = "snippet_detail"),
+    path('<int:snippet_id>/', views.snippet_detail, name='detail'),
     path("<int:snippet_id>/edit/", views.snippet_edit, name = "snippet_edit"),
 ]
--- snippet/views.py
+++ snippet/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect, get_object_or_404
 from snippet.models import Snippet
+from snippet.forms import SnippetForm
 
 def top(request):
     snippet = Snippet.objects.all()
@@ -14,5 +16,36 @@ def snippet_edit(request):
     return HttpResponse('スニペットの編集')
 
 def snippet_detail(request, snippet_id):
-    return HttpResponse('スニペットの詳細閲覧')
+    snippet = get_object_or_404(Snippet , pk = snippet_id)
+    #snippet = Snippet.object.all()
+    return render(request, 'snippet/snippet_detail.html', {'snippet':snippet})
 
+#ログインしていないユーザーがアクセスしたらログイン画面に吹っ飛ばす
+@login_required
+def snippet_new(request):
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_vaild():
+            snippet = form.save(commit = False)
+            snippet.created_by =  request.user 
+            snippet.save()
+            return redirect('snippet_detail', snippet_id = snippet.id)
+    else:
+        form = SnippetForm()
+    return render(request, "snippet/snippet_new.html",{'form':form})
+
+@login_required
+def snippet_edit(request, snippet_id):
+    snippet = get_object_or_404(Snippet , pk = snippet.id)
+    if snippet.created_by.id != request.user.id:
+        return HttpResponseForbidden('Forbidden!!')
+    if request.method == 'POST':
+        form = SnippetForm(request.POST , instance = snippet)
+        if form.is_vaild():
+            form.save()
+            return redirect('snippet_detail', snippet_id = snippet.id)
+    else:
+        form = SnippetForm( instance = snippet)
+    return render(request, "snippet/snippet_edit.html",{'form':form})
+
+        
